3e8c84265f36fe8886062014a23250661e37f4fd
destoswa <84578314+destoswa@users.noreply.github.com>
2023-10-03 14:00:07 +0200

    adaptation_Transformer

--- PROJECT/PointCould_Classification/preprocessing/Create_Dataset.py
+++ PROJECT/PointCould_Classification/preprocessing/Create_Dataset.py
@@ -8,20 +8,22 @@ def MultipleCopies(file_name, folder, num_copies):
         print(f"The file {file_path} can't be found")
         exit()
     for i in range(1, num_copies):
-        copy_name = file_name+str(i)+".pcd"
+        copy_name = file_name+"_"+str(i)+".pcd"
         shutil.copyfile(file_path, folder + "/" + copy_name)
+    os.remove(folder + "/" + file_name + ".pcd")
     print("Copies succesfuly done !")
 
 
 if __name__ == "__main__":
-    os.mkdir("data")
-    os.mkdir("data/garbadge")
-    os.mkdir("data/single")
-    os.mkdir("data/multiple")
-    shutil.copyfile('example.pcd', './data/garbadge/example_garbadge.pcd')
-    MultipleCopies('example_garbadge', './data/garbadge', 100)
-    shutil.copyfile('example.pcd', './data/single/example_single.pcd')
-    MultipleCopies('example_single', './data/single', 100)
-    shutil.copyfile('example.pcd', './data/multiple/example_multiple.pcd')
-    MultipleCopies('example_multiple', './data/multiple', 100)
+    root = "data/modeltrees"
+    os.mkdir(root)
+    os.mkdir(f"{root}/garbadge")
+    os.mkdir(f"{root}/single")
+    os.mkdir(f"{root}/multiple")
+    shutil.copyfile('example.pcd', f'{root}/garbadge/garbadge.pcd')
+    MultipleCopies('garbadge', f'{root}/garbadge', 10000)
+    shutil.copyfile('example.pcd', f'{root}/single/single.pcd')
+    MultipleCopies('single', f'{root}/single', 30000)
+    shutil.copyfile('example.pcd', f'{root}/multiple/multiple.pcd')
+    MultipleCopies('multiple', f'{root}/multiple', 25000)
 
--- PROJECT/PointCould_Classification/preprocessing/preprocess.py
+++ PROJECT/PointCould_Classification/preprocessing/preprocess.py
@@ -2,12 +2,39 @@ import os
 import numpy as np
 from src.pp_pointMLP import pp_pointMLP
 
+def preprocess(source_data, frac_train=.8):
+    # Create file with different labels:
+    with open(f'{source_data}/modeltrees_shape_names.txt', 'w') as f:
+        f.write('garbadge\nmultiple\nsingle')
+
+    # Create references files:
+    num_training_samples = 0
+    num_testing_samples = 0
+    for folder in os.listdir(source_data):
+        if os.path.isdir(source_data + "/" + folder):
+            # partition lists
+            num_files = len(os.listdir(source_data + "/" + folder))
+            num_train = int(num_files * frac_train)
+            num_training_samples += num_train
+            num_testing_samples += num_files - num_train
+            list_train = os.listdir(source_data + "/" + folder)[0:num_train]
+            list_test = os.listdir(source_data + "/" + folder)[num_train::]
+            with open(f'{source_data}/modeltrees_train.txt', 'w') as f:
+                f.write("\n".join(list_train))
+            with open(f'{source_data}/modeltrees_test.txt', 'w') as f:
+                f.write("\n".join(list_test))
+
+    print(f"Final partition for training : {num_training_samples}")
+    print(f"Final partition for testing : {num_testing_samples}")
+
 
 def main():
-    do_pp_pointMLP = True
+    do_pp_pointMLP = False
 
     if do_pp_pointMLP:
         pp_pointMLP("./data", './data_models/pointMLP', .8)
+
+    preprocess("./data/modeltrees", .8)
 
 
 if __name__ == "__main__":
